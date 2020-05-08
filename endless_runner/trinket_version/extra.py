71fcfbcccac5bae874fe8d451b4535bd016b7268
mattjbrooks <mbrooks.github@gmail.com>
2020-05-09 00:27:35 +0100

    Apply same workaround to sound and music subpath

--- endless_runner/trinket_version/extra.py
+++ endless_runner/trinket_version/extra.py
@@ -1,3 +1,5 @@
 import pgzrun
 
 pgzrun.sys.modules['__main__'].__dict__['images'].subpath = ''
+pgzrun.sys.modules['__main__'].__dict__['sounds'].subpath = ''
+pgzrun.sys.modules['__main__'].__dict__['music']._loader.subpath = ''
