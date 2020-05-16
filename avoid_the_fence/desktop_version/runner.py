343ad39fb1a36827822ffd8ca00af075d9c37241
mattjbrooks <mbrooks.github@gmail.com>
2020-05-16 22:38:28 +0100

    Make fall gradually get faster, tidy up comments

--- avoid_the_fence/desktop_version/runner.py
+++ avoid_the_fence/desktop_version/runner.py
@@ -3,9 +3,10 @@ import pgzrun # If using Thonny in Pygame Zero Mode this line is unneeded
 player = Actor("player")
 player.x = 400 # How many pixels to the right player will appear
 player.y = 500 # How many pixels down player will appear
-player.jump = 30
 player.lives = 3
 player.score = 0
+player.jump = 30
+player.fall = 0
 
 cloud = Actor("cloud")
 cloud.pos = 1000, 100 # this is the same as setting cloud.x = 1000 and cloud.y = 100
@@ -41,16 +42,16 @@ def update():
     if player.lives > 0:
         if keyboard.left:
             player.x = player.x - 5 # a shorter way of writing this if you prefer is
-                                        # player.x -= 5
+                                    # player.x -= 5
         elif keyboard.right:
             player.x = player.x + 5 # a shorter way of writing this if you prefer is
-                                        # player.x += 5
+                                    # player.x += 5
         if keyboard.space: # not elif as we may want to move left or right and jump at same time
             jump()
         move_cloud()
         move_fence()
         move_grass()
-        move_player_down()
+        game_gravity()
         check_for_collision()
 
 def jump():
@@ -59,10 +60,9 @@ def jump():
     if player.jump == 30:
         sounds.jump.play()
     player.y = player.y - player.jump
-    # Now we will make player.jump smaller
-    # so the jump won't go on forever if you hold down space.
     if player.jump > 0:
-        player.jump = player.jump - 1
+        player.jump = player.jump - 1  # make player.jump smaller so the jump won't go on
+                                       # forever if space is held down
 
 def move_cloud():
     cloud.x = cloud.x - 1
@@ -80,18 +80,20 @@ def move_grass():
     if grass.x < -100:
         grass.x = 800
 
-def move_player_down():
-    if player.y < 500: # if player is in the air
-        player.y = player.y + 10 # move them down 10 pixels
-    else:
-        player.y = 500 # if player isn't in the air put them on the ground
-        player.jump = 30 # now we're on the ground we can jump again at full power!
+def game_gravity():
+    if player.y < 500:                    # if the player is in the air
+        player.fall = player.fall + 1     # make player.fall bigger so the fall gradually gets faster
+        player.y = player.y + player.fall
+    else:                                 # if the player isn't in the air
+        player.y = 500                    # put them on the ground
+        player.jump = 30                  # now we can jump again, so set this back to 30
+        player.fall = 0                   # and we're no longer falling, so set this back to 0
 
 def check_for_collision():
-    # if player has collided with the center of the fence and isn't a ghost
+    # if the player has collided with the center of the fence and isn't a ghost:
     if player.collidepoint(fence.center) and player.image == "player":
         player.lives = player.lives - 1
-        player.image = "ghost" # change player's image to a transparent ghost
+        player.image = "ghost"         # change player's image to the ghost
         clock.schedule(change_back, 1) # schedule change_back to run 1 second from now
 
 def change_back():
